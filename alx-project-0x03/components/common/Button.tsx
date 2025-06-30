76621f780dea8139a91321da310a0efab7bc16e3
Latera Mekonnen <mekonnenlatera@gmail.com>
2025-06-30 16:00:13 +0300

    programmatic route

--- alx-project-0x03/components/common/Button.tsx
+++ alx-project-0x03/components/common/Button.tsx
@@ -11,17 +11,21 @@ interface ButtonProps {
 const Button = ({ buttonLabel, buttonSize, buttonBackgroundColor, action }: ButtonProps) => {
 
   const backgroundColorClass = buttonBackgroundColor ? {
-    red: 'bg-red-500',
-    blue: 'bg-blue-500',
-    orange: 'bg-orange-500',
-    green: 'bg-green-500',
-    }[buttonBackgroundColor] : 'bg-slate-500'
+       red: 'bg-red-500 hover:bg-red-400',
+       blue: 'bg-blue-500 hover:bg-blue-400',
+      orange: 'bg-orange-500 hover:bg-orange-400',
+      green: 'bg-green-500 hover:bg-green-400',
+    }[buttonBackgroundColor] : 'bg-slate-500 hover:bg-slate-400';
+
 
 
   return (
-    <button onClick={action} className={`${backgroundColorClass} ${buttonSize} px-6 py-2 text-sm font-semibold rounded-lg hover:${backgroundColorClass}/50 transition duration-300 text-white`}>
+    <button onClick={action} className={`${backgroundColorClass} ${buttonSize} px-6 py-2 text-sm font-semibold rounded-lg  transition duration-300 text-white`}>
       {buttonLabel}
     </button>
+
+
+
   )
 }
 
